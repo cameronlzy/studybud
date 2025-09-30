164b7e728dd764ada886517317218b8927b0f513
Cameron <cameronlzy@gmail.com>
2025-09-30 14:18:34 +0800

    Limit activity shown

--- base/views.py
+++ base/views.py
@@ -199,7 +199,7 @@ def topicsPage(request):
     return render(request, 'base/topics.html', context)
 
 def activityPage(request):
-    room_messages = Message.objects.all().order_by('-created')[:5]
+    room_messages = Message.objects.all().order_by('-created')[:4]
     context = {'room_messages': room_messages}
     return render(request, 'base/activity.html', context)
 
